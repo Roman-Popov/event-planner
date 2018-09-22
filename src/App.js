4872b13c20a3ab4ce7efbf7a1a687216dc13cbfb
Roman Popov <popov.r.k@yandex.ru>
2018-09-22 09:00:55 +0300

    feat: Control panel added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-
+                <header className="control-panel">
+                    <button className="btn btn-add">+</button>
+                    <button className="btn btn-select-month">Select month</button>
+                    <Link to="/search" className="btn btn-open-search">Open search</Link>
+                </header>
             </div>
         );
     }
