864c8f0447bd49d3d42f9256cb9deb4d4d0d6cab
Roman Popov <popov.r.k@yandex.ru>
2018-10-12 15:26:43 +0300

    refactor: Header refactor + style changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,51 @@ import { Link } from 'react-router-dom'
 
 class Header extends Component {
     render() {
+        console.log('render cp')
+
         const { currentMonth, currentYear, updateLastSearch } = this.props,
-            buttonText = `${currentMonth}, ${currentYear}`,
-            pathNames = window.location.href.split('/')
+            pathName = window.location.pathname,
+            pages = [
+                {
+                    name: 'Add task',
+                    here: pathName.includes('add-task'),
+                    text: 'Add new task',
+                },
+                {
+                    name: 'Select month',
+                    here: pathName.includes('select-month'),
+                    text: 'Select month',
+                },
+                {
+                    name: 'Day details',
+                    here: pathName.includes('day-details'),
+                    text: `${currentMonth}, ${currentYear}`,
+                },
+                {
+                    name: 'Search',
+                    here: pathName.includes('search'),
+                    text: 'Search by keywords',
+                }
+            ],
+            currentPage = pages.find(elem => elem.here) || '',
+            buttonText = currentPage ? currentPage.text : `${currentMonth}, ${currentYear}`;
 
         return (
             <header className="control-panel">
-                <Link to="/add-task" className="btn btn-add">+</Link>
-
-                {(pathNames.includes('select-month')) ?
-                        <div className="btn btn-select-month disabled">Select month</div> :
-                        (pathNames.includes('day-details')) ?
-                            <div className="btn btn-select-month disabled">{buttonText}</div> :
-                            (pathNames.includes('add-task')) ?
-                                <div className="btn btn-select-month disabled">Add new task</div> :
-                                <Link to="/select-month" className="btn btn-select-month">{buttonText}</Link>
+                {currentPage.name === 'Add task' ?
+                    <div className='btn btn-add disabled'></div> :
+                    <Link to="/add-task" className='btn btn-add'>Add new task</Link>
                 }
 
-                <Link to="/search" className="btn btn-open-search" onClick={() => updateLastSearch('')}>Open search</Link>
+                {currentPage ? // !== Main page
+                    <div className="btn btn-select-month disabled">{buttonText}</div> :
+                    <Link to="/select-month" className="btn btn-select-month">{buttonText}</Link>
+                }
+
+                {currentPage.name === 'Search' ?
+                    <div className="btn btn-open-search disabled"></div> :
+                    <Link to="/search" className="btn btn-open-search" onClick={() => updateLastSearch('')}>Open search</Link>
+                }
             </header>
         )
     }
